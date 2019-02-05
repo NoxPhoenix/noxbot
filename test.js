73f43d05bfcabfadfce68d8a23fed83be83b0f75
NoxPhoenix <mr.mikyo@gmail.com>
2019-02-05 12:01:27 -0500

    remove test script

--- test.js
+++ test.js
@@ -1,15 +1,1 @@
-const names = ['bob', 'jeremy', 'tyler', 'dennison'];
-const names2 = ['l;aksjdlfkjaslkdjasdkf', 'bob', 'jeremy', 'tyler', 'dennison'];
-const names3 = ['ben', 'jon', 'l;aksjdlfkjaslkdjasdkf', 'bob', 'jeremy', 'tyler', 'dennison'];
 
-function winner (list) {
-  let win ='';
-  list.forEach(function (item) {
-    if (item.length > win.length) win = item;
-  });
-  return win;
-}
-
-console.log(winner(names));
-console.log(winner(names2));
-console.log(winner(names3));
